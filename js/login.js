066c95e24756e522ddce816e339c01f4ce38eb0a
omarfarhatul <omarfarhatulhossain@gmail.com>
2025-09-02 18:35:39 +0600

    Updated

--- js/login.js
+++ js/login.js
@@ -1,3 +1,10 @@
-document.getElementById('btn-login').addEventListener('click', function(){
-    console.log('login button click');
+// step-1: set event handler
+document.getElementById('btn-login').addEventListener('click', function(event){
+    // step-2: prevent default behavior (prevent reloading browser)
+    event.preventDefault(); // -->vajal for beginners
+    console.log('login button clicked');
+
+    // step-3: get the phone number
+    const phoneNumber=document.getElementById('phone-number').value;
+    console.log(phoneNumber);
 });
