311479a5df015740c6246780c58262da2b0d4b9c
omarfarhatul <omarfarhatulhossain@gmail.com>
2025-09-03 23:12:51 +0600

    Update cash out

--- js/cashout.js
+++ js/cashout.js
@@ -3,12 +3,20 @@ document.getElementById('btn-cash-out').addEventListener('click', function(event
     console.log('cash out button clicked.');
 
     const cashOut=document.getElementById('input-cash-out').value;
+    const cashOutNumber=parseFloat(cashOut);
+
     const pinNumber=document.getElementById('input-cashout-pin-number').value;
     console.log(cashOut, pinNumber);
     // wrong way to verify pin number
+
     if(pinNumber=== '1234'){
         const balance=document.getElementById('account-balance').innerText;
-        console.log(balance);
+        const balanceNumber=parseFloat(balance);
+
+        // reduce the balance
+        const newBalance=balanceNumber-cashOutNumber;
+        // update the ui
+        document.getElementById('account-balance').innerText=newBalance;
     }
     else{
         alert('Invalid amount or pin. Please try again later.');
