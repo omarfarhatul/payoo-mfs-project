9bb99d9048b373e510e6658fb692749170f5c99a
omarfarhatul <omarfarhatulhossain@gmail.com>
2025-09-03 15:18:17 +0600

    Update account balance

--- js/home.js
+++ js/home.js
@@ -1,7 +1,8 @@
 // add money to the account
 /* 
 s-1: add event handler
-s-2: get money to be added to the account balance
+s-2: get money to be added to the account balance and get pin number
+s-3: verify pin number
 */
 
 // step-1: add an event handler to the add money button inside the form
@@ -16,4 +17,21 @@ document.getElementById('btn-add-money').addEventListener('click', function(even
     // get the pin number provided
     const pinNumberInput= document.getElementById('input-pin-number').value;
     console.log(pinNumberInput);
+
+    // step-3: verify pin number
+    // wrong way to validate pin num
+    if(pinNumberInput==='1234'){
+        console.log('adding money to your account');
+
+        // step-4: get the current balance
+        const balance=document.getElementById('account-balance').innerText;
+        console.log(balance);
+
+        // step-5: add addMoneyInput with balance
+        const newBalance=balance+addMoneyInput;
+        
+    }
+    else{
+        alert('Invalid');
+    }
 });
