570ace662ee17b20beb976611265d9deecbcc177
omarfarhatul <omarfarhatulhossain@gmail.com>
2025-09-04 11:47:44 +0600

    Update for another function

--- js/add-money.js
+++ js/add-money.js
@@ -1,5 +1,16 @@
-document.getElementById('btn-add-money').addEventListener('click', function(){
+document.getElementById('btn-add-money').addEventListener('click', function(event){
+    event.preventDefault();
+
     const addMoney=getInputFieldValueById('input-add-money');
     const pinNumber=getInputFieldValueById('input-pin-number');
-    console.log('Add money inside addMoney.js: ', addMoney, pinNumber);
+    // wrong way to verify
+    if(pinNumber===1234){
+        const balance=getTextFieldValueById('account-balance');
+        const newBalance=balance+addMoney;
+
+        document.getElementById('account-balance').innerText=newBalance;
+    }
+    else{
+        alert('Failed to add the money.');
+    }
 });
--- js/utilities.js
+++ js/utilities.js
@@ -1,6 +1,15 @@
 function getInputFieldValueById(id){
     const inputValue=document.getElementById(id).value;
     const inputNumber=parseFloat(inputValue);
-    console.log(id, inputValue, inputNumber);
+    // console.log(id, inputValue, inputNumber);
+
     return inputNumber;
 }
+
+function getTextFieldValueById(id){
+    const textValue=document.getElementById(id).innerText;
+    const textNumber=parseFloat(textValue);
+
+    return textNumber;
+}
+
